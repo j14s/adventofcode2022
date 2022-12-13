b69993011df91e8ce08771a572cbdfaa11e21d4e
dimes <dimes@j14s.com>
2022-12-13 01:11:39 +0100

    Refactor for cleanliness and a bit more terseness because I couldn't resist

--- 2022/6.py
+++ 2022/6.py
@@ -4,17 +4,15 @@ with open("6-input.txt", 'r') as file:
     input = file.read().rstrip()
 
 def search_signal(start, length):
-    start_position = start
     for pos in input:
-        start_position_end = start_position + length
-        this_four = list(input[start_position:start_position_end])
-        start_position += 1
+        end = start + length
+        this_four = list(input[start:end])
+        start += 1
         if len(this_four) == len(set(this_four)):
-            return(start_position_end)
+            return(end)
 
-signal_position_end = search_signal(0, 4)
-print(signal_position_end)
+packet = search_signal(0, 4)
+print(packet)
 
-message_position_end = search_signal(signal_position_end, 14)
-print(message_position_end)
-
+message = search_signal(packet, 14)
+print(message)
